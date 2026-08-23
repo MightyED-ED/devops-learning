c8de367edd4f34709880c06f523709f1583a52a1
MightyED-ED <eathon.davids.ed@gmail.com>
2026-08-23 22:50:46 +0200

    Release application version 2

---
+++ application/app.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "==========================="
+echo " DevOps Learning Application"
+echo " Version 2.0"
+echo "==========================="
+echo "Application version 2  is running"
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
+
 echo "starting deployment..."
-echo "checking application..."
-echo "application deployed!"
+
+SOURCE="$HOME/devops-lab/application/app.sh"
+DEST="/opt/devops-app/app.sh"
+
+echo "copying application..."
+
+sudo cp "$SOURCE" "$DEST"
+
+echo "setting permissions..."
+
+sudo chown root:developers "$DEST"
+sudo chmod 770 "$DEST"
+
 echo "deployment complete!"
+
+echo "testing application..."
+
+sudo "$DEST"
